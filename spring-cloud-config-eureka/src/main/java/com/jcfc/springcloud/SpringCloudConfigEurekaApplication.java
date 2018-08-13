bad39384ab4f7a94667030943db8a05ba62970f4
yangdaobin <yangdaobin@jcfc.cn>
2018-08-13 17:33:20 +0800

    config-eureka/provider

---
+++ spring-cloud-config-eureka/src/main/java/com/jcfc/springcloud/SpringCloudConfigEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.jcfc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudConfigEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigEurekaApplication.class, args);
+	}
+}
---
+++ spring-cloud-config-provider/src/main/java/com/jcfc/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,32 @@
+package com.jcfc.springcloud.service.impl;
+
+import com.jcfc.springcloud.entity.Dept;
+import com.jcfc.springcloud.mapper.DeptMapper;
+import com.jcfc.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+
+    @Autowired
+    private DeptMapper dao;
+
+    @Override
+    public boolean add(Dept dept) {
+        return dao.addDept(dept);
+    }
+
+    @Override
+    public Dept get(Long id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    public List<Dept> list() {
+        return dao.findAll();
+    }
+
+}
